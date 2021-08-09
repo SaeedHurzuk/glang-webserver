626996a78616cc9dd24b8560b426950fa57d73b7
SaeedHurzuk <saeedhurzuk@icloud.com>
2021-08-09 08:24:12 +0200

    Added Empty Cert And get IP func

--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	f "fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -53,13 +55,24 @@ func main() {
 
 	// Clear any previous data in console
 	clearConsole()
+	ip, err := externalIP()
+	if err != nil {
+		f.Println(err)
+	}
+	f.Println(ip)
 	f.Println("######### GoLang Webserver Configuration #########")
 	f.Print("Enter Host: ")
 	f.Scan(&host)
 	f.Print("Enter Port (80|443): ")
 	f.Scan(&port)
 	// set url var with host and port
-	url = host + ":" + port
+	if port == "443" {
+		url = "https://" + host + ":" + port
+	} else if port == "80" {
+		url = "http://" + host + ":" + port
+	} else {
+		url = "http://" + host + ":" + port
+	}
 	clearConsole()
 
 	// If conncted display connection details
@@ -67,7 +80,7 @@ func main() {
 	f.Println("GoLang Server Started")
 	f.Println("Host: " + host)
 	f.Println("Port: " + port)
-	f.Println("URL: http://" + url)
+	f.Println("URL: " + url)
 	f.Println("#################### GoLang Webserver ####################")
 
 	// check if user hits ctrl + c
@@ -83,12 +96,13 @@ func main() {
 				clearConsole()
 				f.Println("#################### GoLang Webserver ####################")
 				log.Println("Stopping GoLang Server and exiting..")
-				f.Println("#################### GoLang Webserver ####################")
 				if oSystem != "windows" {
 					pprof.StopCPUProfile()
+					f.Println("#################### GoLang Webserver ####################")
 					os.Exit(1)
 				} else {
-					log.Fatal("Please Check Your PORTS")
+					f.Println("#################### GoLang Webserver ####################")
+					os.Exit(1)
 				}
 			} else {
 				clearConsole()
@@ -96,21 +110,36 @@ func main() {
 				f.Println("GoLang Server Started")
 				f.Println("Host: " + host)
 				f.Println("Port: " + port)
-				f.Println("URL: http://" + url)
+				f.Println("URL: " + url)
 				f.Println("#################### GoLang Webserver ####################")
 			}
 		}
 	}()
 	// http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
 	// http.ListenAndServe(host+":"+port, nil)
-	if err := http.ListenAndServeTLS(host+":"+port, "./certificates/server.crt", "./certificates/server.key", nil); err != nil {
-		clearConsole()
-		f.Println("#################### GoLang Webserver ####################")
-		f.Print(t.Format("2006-01-02 15:04:05"))
-		f.Print(" Unable to connect to PORT " + port)
-		f.Print("\r\n#################### GoLang Webserver ####################\r\n")
-		if oSystem == "windows" {
-			os.Exit(1)
+	if port == "443" {
+		// listen and server HTTPs
+		if err := http.ListenAndServeTLS(host+":"+port, "./certificates/server.crt", "./certificates/server.key", nil); err != nil {
+			clearConsole()
+			f.Println("#################### GoLang Webserver ####################")
+			f.Print(t.Format("2006-01-02 15:04:05"))
+			f.Print(" Unable to connect to PORT " + port)
+			f.Print("\r\n#################### GoLang Webserver ####################\r\n")
+			if oSystem == "windows" {
+				os.Exit(1)
+			}
+		}
+	} else {
+		// listen via http on any other port other than 443
+		if err := http.ListenAndServe(host+":"+port, nil); err != nil {
+			clearConsole()
+			f.Println("#################### GoLang Webserver ####################")
+			f.Print(t.Format("2006-01-02 15:04:05"))
+			f.Print(" Unable to connect to PORT " + port)
+			f.Print("\r\n#################### GoLang Webserver ####################\r\n")
+			if oSystem == "windows" {
+				os.Exit(1)
+			}
 		}
 	}
 }
@@ -124,4 +153,41 @@ func strstr(haystack string, needle string) string {
 		return ""
 	}
 	return haystack[idx+len([]byte(needle))-1:]
+}
+
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
 }
